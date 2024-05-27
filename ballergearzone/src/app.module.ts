81e7b992b820171598239c8d41d5bc9a7393ac63
ThiagoSchiaffino <thiagoschiaffinodechia@gmail.com>
2024-05-27 20:10:46 -0300

    Login Service

--- ballergearzone/src/app.module.ts
+++ ballergearzone/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { CommonModule } from './common/common.module';
+import { AdministradorModule } from './administrador/administrador.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsuarioModule, CommonModule, AdministradorModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
